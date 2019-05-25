7e79824a530f3c57883dfb2823a3677ad8063026
silverio <silverio@silverio-VirtualBox>
2019-05-25 14:21:21 +0200

    Actualización y corrección de varias clases

--- Market/src/main/java/Market_BL/ProductosBL.java
+++ Market/src/main/java/Market_BL/ProductosBL.java
@@ -6,68 +6,49 @@ import java.util.List;
 
 import Market_DO.Producto;
 
+/**
+ * Clase para la optención de una lista de productos.
+ * @author silverio
+ */
 public class ProductosBL {
 	
-	public static List<Producto> darProductos()
-	{
+	
+	/**
+	 * Función para la creación de una lista de productos buscada. Puede contener una cadena 
+	 * para realizar un filtro o un valor nulo para obtener una lista completa.
+	 * @param filterProducto filtro a aplicar, puede ser un valor null o contener una cadena texto.
+	 * @return Lista final de productos
+	 */
+	public static List<Producto> darProductos(String filterProducto) {
 		try {
-			ResultSet resultSet = Market_DA.ProductoDA.darProductos();
-			
-			List<Producto> listProductos;
-			
-			 Producto item = null;
-			 listProductos = new ArrayList<Producto>();
-			 
-			 while (resultSet.next()) {
-				 item = new Producto();
-				 item.setId(resultSet.getInt("Id"));
-				 item.setCategoria_Id(resultSet.getInt("Categoria_Id"));
-				 item.setMarca_Id(resultSet.getInt("Marca_Id"));
-				 item.setTienda_Id(resultSet.getInt("Tienda_Id"));
-				 item.setImagen(resultSet.getString("Imagen"));
-				 item.setDescripcion(resultSet.getString("Descripcion"));
-				 item.setCantidad(resultSet.getInt("Cantidad"));
-				 item.setPrecio(resultSet.getDouble("Precio"));
-				 
-				 listProductos.add(item);
-			 }
-			 
-			 return listProductos;
-			 
+			ResultSet resultSet;
+			Producto item = null;
+			//Lista de productos
+			List<Producto> listProductos = new ArrayList<Producto>();
+			//Si se recibe null como parámetro, no un filtro previo.
+			if (filterProducto == null) {resultSet = Market_DA.ProductoDA.darProductos();} 
+			//En otro caso se atiende al filtro para crear la la lista que contiene los productos
+			else {resultSet = Market_DA.ProductoDA.darProductos(filterProducto);}
+			//Bucle para añadir items		
+			while (resultSet.next()) {
+				/*comprobar que se obtiene de alguna forma lo que se necesita. */
+				item = new Producto();
+				item.setId(resultSet.getInt("Id"));
+				item.setCategoria_Id(resultSet.getInt("Categoria_Id"));
+				item.setMarca_Id(resultSet.getInt("Marca_Id"));
+				item.setTienda_Id(resultSet.getInt("Tienda_Id"));
+				item.setImagen(resultSet.getString("Imagen"));
+				item.setDescripcion(resultSet.getString("Descripcion"));
+				item.setCantidad(resultSet.getInt("Cantidad"));
+				item.setPrecio(resultSet.getDouble("Precio"));
+				//Añade el item a la lista de productos
+				listProductos.add(item);
+			}
+			return listProductos;
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
-		}	
+		}
 	}
-	
-	public static List<Producto> darProductos(String filterProducto)
-	{
-		try {
-			ResultSet resultSet = Market_DA.ProductoDA.darProductos(filterProducto);
-			
-			List<Producto> listProductos;
-			
-			 Producto item = null;
-			 listProductos = new ArrayList<Producto>();
-			 
-			 while (resultSet.next()) {
-				 item = new Producto();
-				 item.setId(resultSet.getInt("Id"));
-				 item.setCategoria_Id(resultSet.getInt("Categoria_Id"));
-				 item.setMarca_Id(resultSet.getInt("Marca_Id"));
-				 item.setTienda_Id(resultSet.getInt("Tienda_Id"));
-				 item.setImagen(resultSet.getString("Imagen"));
-				 item.setDescripcion(resultSet.getString("Descripcion"));
-				 item.setCantidad(resultSet.getInt("Cantidad"));
-				 item.setPrecio(resultSet.getDouble("Precio"));
-				 
-				 listProductos.add(item);
-			 }		 
-			 return listProductos;		 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return null;
-		}	
-	}
-	
+
 }
